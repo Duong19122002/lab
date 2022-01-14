7201a3beb0c87406d7b71066e5b928ab157a94ad
Duong19122002 <duong19122002@gmail.com>
2022-01-14 19:16:23 +0700

    save

--- src/components/news.js
+++ src/components/news.js
@@ -8,7 +8,7 @@ const News = {
                 ${data.map((post) => `
                     <div class="border p-3">
                         <img src="${post.img}" alt="" />
-                        <h3><a href="/product/${post.id}">${post.title}</a></h3>
+                        <h3 class="my-3 font-semibold text-orange-500 text-lg"><a href="/product/${post.id}">${post.title}</a></h3>
                         <p>${post.desc}</p>
                     </div>  
                     `).join("")}
