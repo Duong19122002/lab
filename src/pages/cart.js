09cc5009ccf4736a4273be22af702bb8a5707123
Duong19122002 <duong19122002@gmail.com>
2022-02-21 10:03:36 +0700

    edit cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -85,46 +85,12 @@ const CartPage = {
       <div class="my-4 mt-6 -mx-2 lg:flex">
         <div class="lg:px-2 lg:w-1/2">
           
-          <div class="p-4">
-            <p class="mb-4 italic">If you have some information for the seller you can leave them in the box below</p>
-            <textarea class="w-full h-24 p-2 bg-gray-100 rounded"></textarea>
-          </div>
+          
         </div>
         <div class="lg:px-2 lg:w-1/2">
           
           <div class="p-4">
-              <div class="flex justify-between border-b">
-                <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                  Subtotal
-                </div>
-                <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                  148,827.53€
-                </div>
-              </div>
-                <div class="flex justify-between pt-4 border-b">
-                  <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                    New Subtotal
-                  </div>
-                  <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                    14,882.75€
-                  </div>
-                </div>
-                <div class="flex justify-between pt-4 border-b">
-                  <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                    Tax
-                  </div>
-                  <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                    2,976.55€
-                  </div>
-                </div>
-                <div class="flex justify-between pt-4 border-b">
-                  <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                    Total
-                  </div>
-                  <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                    17,859.3€
-                  </div>
-                </div>
+              
               <a href="#">
                 <button class="flex justify-center w-full px-10 py-3 mt-6 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
                   <svg aria-hidden="true" data-prefix="far" data-icon="credit-card" class="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z"/></svg>
